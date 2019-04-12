ef8a68d91651827c06a12ea0a469aefb0d381966
Jamie Toloui <jamie@toloui.com>
2019-04-12 17:24:24 +0100

    Updates

---
+++ precache-manifest.c1108d2e0bdb6aa049fdead2e2fc34c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44515f7718994a1da600",
+    "url": "/Video-Player/static/js/runtime~main.e13d3bc1.js"
+  },
+  {
+    "revision": "8a977ad86a3ae4c12362",
+    "url": "/Video-Player/static/js/main.7bfbbec9.chunk.js"
+  },
+  {
+    "revision": "381bd96f99f3fb8fb88d",
+    "url": "/Video-Player/static/js/2.c79a505c.chunk.js"
+  },
+  {
+    "revision": "8a977ad86a3ae4c12362",
+    "url": "/Video-Player/static/css/main.f2d9a295.chunk.css"
+  },
+  {
+    "revision": "7632fd4b55edad38f01714eca07f089b",
+    "url": "/Video-Player/index.html"
+  }
+];
