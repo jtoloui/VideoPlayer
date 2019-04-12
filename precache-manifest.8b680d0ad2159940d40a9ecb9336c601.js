0e9bb1f12356d6b21869d903fa5df1905d275578
Jamie Toloui <jamie@toloui.com>
2019-04-12 17:50:51 +0100

    Updates

---
+++ precache-manifest.8b680d0ad2159940d40a9ecb9336c601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "313474e632ec191bdb78",
+    "url": "/VideoPlayer/static/js/runtime~main.98d3d01d.js"
+  },
+  {
+    "revision": "8a977ad86a3ae4c12362",
+    "url": "/VideoPlayer/static/js/main.7bfbbec9.chunk.js"
+  },
+  {
+    "revision": "381bd96f99f3fb8fb88d",
+    "url": "/VideoPlayer/static/js/2.c79a505c.chunk.js"
+  },
+  {
+    "revision": "8a977ad86a3ae4c12362",
+    "url": "/VideoPlayer/static/css/main.f2d9a295.chunk.css"
+  },
+  {
+    "revision": "2983b4c3866d459dffa4a773d13850aa",
+    "url": "/VideoPlayer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Video-Player/precache-manifest.c1108d2e0bdb6aa049fdead2e2fc34c9.js"
+  "/VideoPlayer/precache-manifest.8b680d0ad2159940d40a9ecb9336c601.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Video-Player/index.html", {
+workbox.routing.registerNavigationRoute("/VideoPlayer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
