35d0d713f6b9ef2366cb134684978d592a966b4c
Jamie Toloui <jamie@toloui.com>
2019-04-17 12:45:35 +0100

    Updates

--- precache-manifest.35a2049c96b299eed920abc9694c035a.js
+++ precache-manifest.35a2049c96b299eed920abc9694c035a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/VideoPlayer/static/js/runtime~main.98d3d01d.js"
   },
   {
-    "revision": "a28b69c041df3f96aaf0",
-    "url": "/VideoPlayer/static/js/main.01204988.chunk.js"
+    "revision": "56d88e2f1883b3e27099",
+    "url": "/VideoPlayer/static/js/main.56115dcb.chunk.js"
   },
   {
     "revision": "381bd96f99f3fb8fb88d",
     "url": "/VideoPlayer/static/js/2.c79a505c.chunk.js"
   },
   {
-    "revision": "a28b69c041df3f96aaf0",
+    "revision": "56d88e2f1883b3e27099",
     "url": "/VideoPlayer/static/css/main.dc1a7ec0.chunk.css"
   },
   {
-    "revision": "9af6df327d1768624d33b342269d4072",
+    "revision": "c8afa065725d69a2ce1e6d0b24f3365c",
     "url": "/VideoPlayer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/VideoPlayer/precache-manifest.9be4692578de34b9a1d28526466a7b33.js"
+  "/VideoPlayer/precache-manifest.35a2049c96b299eed920abc9694c035a.js"
 );
 
 workbox.clientsClaim();
