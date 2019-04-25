81a7bcfcc10d6f8c451f86ef4b286eb8ffbe4c88
Jamie Toloui <jamie@toloui.com>
2019-04-25 20:47:06 +0100

    test

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,10 +3,7 @@ import React from "react";
 
 const VideoItem = ({ video, onVideoSelect }) => {
 	return (
-		<div
-			onClick={() => onVideoSelect(video)}
-			className="video-item item"
-		>
+		<div onClick={() => onVideoSelect(video)} className="video-item item">
 			<img
 				className="ui image"
 				alt={video.snippet.description}
