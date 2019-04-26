264eaef9c7bdbbd6ee799ec83b627cc46f58d5b2
Jamie Toloui <jamie@toloui.com>
2019-04-26 13:11:54 +0100

    Updates

---
+++ precache-manifest.57961b658d1dc24e56e6a48763b3468a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6790c3d06212d8d184823379209a1a4d",
+    "url": "/VideoPlayer/index.html"
+  },
+  {
+    "revision": "bbb3fb041e80bccf11ee",
+    "url": "/VideoPlayer/static/css/main.5cd10557.chunk.css"
+  },
+  {
+    "revision": "7f2d52fe749de013be8d",
+    "url": "/VideoPlayer/static/js/2.b70d9ed3.chunk.js"
+  },
+  {
+    "revision": "bbb3fb041e80bccf11ee",
+    "url": "/VideoPlayer/static/js/main.f6754533.chunk.js"
+  },
+  {
+    "revision": "44a7f5555d4677334730",
+    "url": "/VideoPlayer/static/js/runtime~main.168de462.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/VideoPlayer/precache-manifest.2eca772ef0b58e00cd1959902b6da592.js"
+  "/VideoPlayer/precache-manifest.57961b658d1dc24e56e6a48763b3468a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/VideoPlayer/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/VideoPlayer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
