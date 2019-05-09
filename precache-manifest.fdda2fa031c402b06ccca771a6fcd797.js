7ccc418da67dccb3b2dbe55026c7aa69986a21ac
Jamie Toloui <jamie@toloui.com>
2019-05-09 20:39:50 +0100

    Updates

---
+++ precache-manifest.fdda2fa031c402b06ccca771a6fcd797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a7f5555d4677334730",
+    "url": "/VideoPlayer/static/js/runtime~main.168de462.js"
+  },
+  {
+    "revision": "8b5f0cdafbff70be62b7",
+    "url": "/VideoPlayer/static/js/main.92829b57.chunk.js"
+  },
+  {
+    "revision": "dc4b33ee0cb58b638ebe",
+    "url": "/VideoPlayer/static/js/2.456e39e9.chunk.js"
+  },
+  {
+    "revision": "8b5f0cdafbff70be62b7",
+    "url": "/VideoPlayer/static/css/main.b8d1ad03.chunk.css"
+  },
+  {
+    "revision": "ecfdacc5671a707e4c70ba717ce941f4",
+    "url": "/VideoPlayer/index.html"
+  }
+];
